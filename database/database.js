47b914de5990bdc5f0ba054bc9784d9a551061d0
ASHOK SASITHARAN <asasitharan28@gmail.com>
2026-02-04 22:02:15 -0500

    Made small change to docker file

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.resolve(__dirname, 'badminton.db'),
+  storage: path.resolve(__dirname, 'data', 'badminton.db'),
 });
 module.exports = sequelize;
 
